d1008aa8453252b85d56871e4e0b6b798636a93e
Brian Soriano Dev. <163360612+UnknownXD3@users.noreply.github.com>
2025-01-13 08:30:19 -0400

    Add files via upload

---
+++ Simples.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //EJERCICIO 1                                                           1
+
+            //int a, b, suma, resta, multiplicacion, division;
+
+            //Console.WriteLine("Escribe el 1mer número");
+            //a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Escribe el 2do número");
+            //b = int.Parse(Console.ReadLine());
+
+            //suma = a + b;
+            //resta = b - a;
+            //multiplicacion = a * b;
+            //division = a / b;
+
+            //Console.WriteLine("La suma de estos dos números da: " + suma.ToString());
+            //Console.WriteLine("La resta de estos dos números da: " + resta.ToString());
+            //Console.WriteLine("La multiplicación de estos dos números da: " + multiplicacion.ToString());
+            //Console.WriteLine("La división de estos dos números da: " + division.ToString());
+            //Console.ReadKey();
+
+
+
+
+
+
+
+
+            //EJERCICIO 2                                                                    2
+
+            //decimal sueldo_neto, sueldo_bruto, descuento, pago_hora, horas_trabajadas, afp, sfs;
+
+            //Console.WriteLine("Ingrese de cuanto es el pago por hora: ");
+            //pago_hora = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingrese cuantas horas trabajó el empleado: ");
+            //horas_trabajadas = int.Parse(Console.ReadLine());
+
+            //sueldo_bruto = pago_hora * horas_trabajadas;
+            //afp = sueldo_bruto * 0.0287m;
+            //sfs = sueldo_bruto * 0.0304m;
+            //descuento = afp + sfs;
+            //sueldo_neto = sueldo_bruto - descuento;
+
+            //Console.WriteLine("El Sueldo Neto del empleado es: " + sueldo_neto.ToString());
+            //Console.ReadKey();
+
+
+
+
+
+
+
+
+
+            //EJERCICIO 3                                                                    3
+
+            //double a, b, c, suma, resta, divide, multiplica;
+            //Console.WriteLine("Primer valor: ");
+            //a = double.Parse(Console.ReadLine());
+            //Console.WriteLine("segundo valor: ");
+            //b = double.Parse(Console.ReadLine());
+            //Console.WriteLine("tercer valor: ");
+            //c = double.Parse(Console.ReadLine());
+
+            //suma = a + b + c;
+            //resta = a - b - c;
+            //divide = a / b / c;
+            //multiplica = a * b * c;
+
+            //Console.WriteLine("La suma de estos tres números da: " + suma.ToString());
+            //Console.WriteLine("La resta de estos tres números da: " + resta.ToString());
+            //Console.WriteLine("La divición de estos tres números da: " + divide.ToString());
+            //Console.WriteLine("La multiplicación de estos tres números da: " + multiplica.ToString());
+            //Console.ReadKey();
+
+
+
+
+
+
+
+
+
+            //EJERCICIO 4                                                               4
+
+            //int promedio, n1, n2, n3, n4, n5;
+
+            //Console.WriteLine("Valor 1:");
+            //n1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("valor 2: ");
+            //n2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("valor 3: ");
+            //n3 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("valor 4: ");
+            //n4 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("valor 5: ");
+            //n5 = int.Parse(Console.ReadLine());
+
+            //promedio = (n1 + n2 + n3 + n4 + n5) / 5;
+
+            //Console.WriteLine("El promedio de estos 5 valores es: " + promedio.ToString());
+            //Console.ReadKey();
+
+
+
+
+
+
+
+
+            //EJERCICIO 5                                                                       5
+            //int precio, cantidad, importe;
+
+            //Console.WriteLine("Ingrese el precio unitario del artículo: ");
+            //precio = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingrese la cantidad del artículo a comprar: ");
+            //cantidad = int.Parse(Console.ReadLine());
+
+            //importe = precio * cantidad;
+
+            //Console.WriteLine("El importe del artículo es:" + importe.ToString());
+            //Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+            //EJERCICIO 1                                                         6
+
+            //    double precio, cantidad, importe, itbis, descuento, total;
+
+            //Console.WriteLine("Ingrese el precio del artículo: ");
+            //precio = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingrese la cantidad del artículo a comprar: ");
+            //cantidad = int.Parse(Console.ReadLine());
+
+            //importe = precio * cantidad;
+            //itbis = importe * 0.18;
+            //descuento = importe * 0.10;
+            //total = importe - descuento + itbis;
+
+            //Console.WriteLine("El importe es: " + importe.ToString());
+            //Console.WriteLine("El ITBIS es: " + itbis.ToString());
+            //Console.WriteLine("El Descuento es: " + descuento.ToString());
+            //Console.WriteLine("El Total general es: " + total.ToString());
+
+            //Console.ReadKey();
+
+
+
+
+
+
+
+
+
+            //EJERCICIO 2                                                                  7
+
+            //double sueldo_emp, aumento, total;
+
+
+            //Console.WriteLine("¿Cúal es el sueldo actual del empleado?");
+            //sueldo_emp = double.Parse(Console.ReadLine());
+
+            //aumento = 0.15;
+
+            //total = (sueldo_emp * aumento) + sueldo_emp;
+
+
+            //Console.WriteLine("El aumento es "+aumento.ToString());
+            //Console.WriteLine("El sueldo final del empleado es de: " + total.ToString());
+            //Console.ReadKey();
+        }
+    }
+}
